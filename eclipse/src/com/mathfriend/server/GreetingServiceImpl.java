0a829505388be197fc8d3f47453ed3ab6ddff538
roman <roman@X301.(none)>
2012-10-08 02:04:48 -0400

    inserted java into server. Parse equations

--- eclipse/src/com/mathfriend/server/GreetingServiceImpl.java
+++ eclipse/src/com/mathfriend/server/GreetingServiceImpl.java
@@ -1,8 +1,10 @@
 package com.mathfriend.server;
 
-import com.mathfriend.client.GreetingService;
-import com.mathfriend.shared.FieldVerifier;
+import tree.Term;
+import tree.compound.CompoundTerm;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.mathfriend.client.GreetingService;
 
 /**
  * The server side implementation of the RPC service.
@@ -12,14 +14,7 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 		GreetingService {
 
 	public String greetServer(String input) throws IllegalArgumentException {
-		// Verify that the input is valid. 
-		if (!FieldVerifier.isValidName(input)) {
-			// If the input is not valid, throw an IllegalArgumentException back to
-			// the client.
-			throw new IllegalArgumentException(
-					"Name must be at least 4 characters long");
-		}
-
+	
 		String serverInfo = getServletContext().getServerInfo();
 		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
 
@@ -27,8 +22,9 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 		input = escapeHtml(input);
 		userAgent = escapeHtml(userAgent);
 
-		return "Hello, " + input + "!<br><br>I am running " + serverInfo
-				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+		CompoundTerm term = (CompoundTerm)Term.getTermFromString(input);
+		
+		return term.toString();
 	}
 
 	/**
